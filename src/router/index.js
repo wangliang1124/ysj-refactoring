b318512cdbb0ca36c8d00aefeb2021e9999e4459
anotherleon <wangliang1124@163.com>
2017-12-25 22:23:08 +0800

    beforeEach

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,58 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import cookie from 'cookiejs'
 import HelloWorld from 'components/HelloWorld'
 import Home from 'components/Home'
 import FlexibleTest from 'components/FlexibleTest'
 
 Vue.use(Router)
-
-export default new Router({
+const routes = [
+  {
+    path: '/',
+    component: Home,
+    name: 'home',
+    meta: {
+      title: 'HOME',
+    },
+    beforeEnter(to, from, next) { console.log('==========home route========'); next() },
+  },
+  {
+    path: '/hello',
+    component: HelloWorld,
+    name: 'hello',
+    meta: {
+      title: 'hello',
+    },
+    beforeEnter(to, from, next) {
+      console.log('=================测试e===================')
+      console.log(HelloWorld)
+      next()
+    },
+  },
+  {
+    path: '/flexibletest',
+    component: FlexibleTest,
+    name: 'flexibleTest',
+  },
+]
+const router = new Router({
   mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-      meta: {
-        title: 'HOME',
-      },
-      beforeEnter(to, from, next) { console.log('==========home route========'); next() },
-    },
-    {
-      path: '/hello',
-      name: 'hello',
-      component: HelloWorld,
-      meta: {
-        title: 'hello',
-      },
-      beforeEnter(to, from, next) {
-        console.log('=================测试e===================')
-        console.log(HelloWorld)
-        next()
-      },
-    },
-    {
-      path: '/flexibletest',
-      name: 'flexibleTest',
-      component: FlexibleTest,
-    },
-  ],
+  routes,
 })
+router.beforeEach(async (to, from, next) => {
+  console.log(to, from)
+  console.log(to.matched)
+  const isAuth = !!cookie('token')
+  // const host = location.search;
+  const { code, inviter_id } = to.query
+  // const { requireAuth = false, isAuth } = router
+  if (isAuth) {
+    next()
+    return
+  }
+  if (code) {
+    console.log()
+  }
+})
+
+export default router
